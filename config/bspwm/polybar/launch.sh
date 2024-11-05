37a618576d9619ad2f270a4bc039045e67b5e96a
skilax <132996178+sk1lax@users.noreply.github.com>
2024-11-05 22:04:38 +0300

    Add files via upload

---
+++ config/bspwm/polybar/launch.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+pkill polybar && sleep 1
+polybar --config=~/.config/bspwm/polybar/config.ini
+
---
+++ config/bspwm/rofi/scripts/launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Current Theme
+rofi -show drun -icon-theme Tela-black -theme /home/skilax/.config/bspwm/rofi/launcher.rasi &
